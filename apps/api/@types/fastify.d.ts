c2e730e1ab1946fe0824fe56f53c09461debbaff
Diogo Silva <diogomfc@hotmail.com>
2024-05-21 10:24:22 -0300

    feat(middleware): Adiciona uma nova função de middleware  para recuperar informações de associação para um slug de organização fornecido

--- apps/api/@types/fastify.d.ts
+++ apps/api/@types/fastify.d.ts
@@ -1,7 +1,12 @@
 import 'fastify'
 
+import { Member, Organization } from '@prisma/client'
+
 declare module 'fastify' {
   export interface FastifyRequest {
     getCurrentUserId(): Promise<string> // const { sub } = await request.jwtVerify<{ sub: string }>()
+    getUserMembership(
+      slug: string,
+    ): Promise<{ organization: Organization; membership: Member }>
   }
 }
