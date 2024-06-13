fc5296e9fc20e6fcc0f50b0faaff0efbe0e3f3f1
Diogo Silva <diogomfc@hotmail.com>
2024-06-13 15:41:51 -0300

    feat(web-page-auth): Realiza login do usuário

--- apps/web/src/app/auth/sign-in/sign-in-form.tsx
+++ apps/web/src/app/auth/sign-in/sign-in-form.tsx
@@ -3,6 +3,7 @@
 import { AlertTriangle, Loader2 } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 import githubIcon from '@/assets/github-icon.svg'
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
@@ -15,6 +16,8 @@ import { useFormState } from '@/hooks/use-form-state'
 import { signInWithEmailAndPassword } from './actions'
 
 export function SignInForm() {
+  const router = useRouter()
+
   // const [{ errors, message, success }, formAction, isPending] = useActionState(
   //   signInWithEmailAndPassword,
   //   { success: false, message: null, errors: null },
@@ -22,6 +25,9 @@ export function SignInForm() {
 
   const [{ errors, message, success }, handleSubmit, isPending] = useFormState(
     signInWithEmailAndPassword,
+    () => {
+      router.push('/')
+    },
   )
 
   // async function handleSignIn(event: FormEvent<HTMLFormElement>) {
