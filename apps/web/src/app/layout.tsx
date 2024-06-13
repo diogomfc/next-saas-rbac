039dc7ace13df4cd4015897045ddc14b943be7dd
Diogo Silva <diogomfc@hotmail.com>
2024-06-13 19:07:10 -0300

    feat(web-components): Temas dark/light com Next Themes

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="pt-BR" className="dark">
+    <html lang="pt-BR" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <ThemeProvider
           attribute="class"
